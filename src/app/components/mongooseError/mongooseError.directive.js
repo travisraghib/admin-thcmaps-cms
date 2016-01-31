6a73a1c2d26a707cfeb842f02ab24a18c34f7d76
Travis Raghib <travisraghib@gmail.com>
2016-01-30 23:13:34 -0800

    adding mongoose errors

---
+++ src/app/components/mongooseError/mongooseError.directive.js
@@ -0,0 +1,12 @@
+export function MongooseError() {
+    'ngInject';
+
+    let directive = {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModel) {
+            element.on('keydown', () => ngModel.$setValidity('mongoose', true));
+        }
+    };
+    return directive;
+}

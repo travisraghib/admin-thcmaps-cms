225add921882c8e1c3bc37cc9fd36219fbf07f35
Travis Raghib <travisraghib@gmail.com>
2016-02-01 22:34:19 -0800

    added resolve to select

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -11,7 +11,12 @@ export function routerConfig($stateProvider, $urlRouterProvider) {
             url : '/select',
             templateUrl : 'app/select/select.html',
             controller  : 'SelectController',
-            controllerAs: 'select'
+            controllerAs: 'select',
+            resolve     : {
+                vendorList: (vendorDataService) =>{
+                    return vendorDataService.getVendorList()
+                }
+            }
         })
         .state('main', {
             url         : '/manage/:id',

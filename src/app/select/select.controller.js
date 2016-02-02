6f0abf3ab39d69d57d7f6ddbc5bf5fa685cf00bf
Travis Raghib <travisraghib@gmail.com>
2016-02-01 22:36:45 -0800

    modified add and delete business

--- src/app/select/select.controller.js
+++ src/app/select/select.controller.js
@@ -1,23 +1,43 @@
 export class SelectController {
-    constructor($log, vendorDataService) {
+    constructor($log, $state, vendorDataService, vendorList, _) {
         'ngInject';
         //debug
         this.log = $log.log;
         this.error = $log.error;
 
         //deps
+        this.$state = $state;
         this.vendorDataService = vendorDataService;
 
-        //template data
-        this.business = this.vendorDataService.business;
+        //view template data
+        this.vendorList = vendorList;
+        console.log(vendorList);
     }
 
+    //create new business
     addBusiness(){
-        this.log(16);
         this.vendorDataService.addBusiness()
-            .$promise
-            .then(function(res){
-                this.log(res);
-            });
+            .then(data=>{
+                let _id = data._id;
+                this.vendorList.unshift({_id});
+            })
+            .catch(err =>{
+                //this.$state.go('login')
+            })
+    }
+
+    //delete existing business
+    deleteBusiness(id) {
+        this.log(id);
+        this.vendorDataService.deleteBusiness(id)
+            .then(data => {
+                _.forEach(this.vendorList, (vendor, index )=>{
+                    this.log(vendor);
+                    if(vendor._id === id){
+                        this.vendorList.splice(index, 1);
+                        return false;
+                    }
+                })
+            })
     }
 }

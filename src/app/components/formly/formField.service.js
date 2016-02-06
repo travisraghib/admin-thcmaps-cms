ee37c3321f64b8186aad334a55497d318c4e8df9
Travis Raghib <travisraghib@gmail.com>
2016-02-06 14:30:15 -0800

    formatting

--- src/app/components/formly/formField.service.js
+++ src/app/components/formly/formField.service.js
@@ -1,128 +1,15 @@
-export class formFieldService{
-    constructor (vendorDataService, constants, _){
+export class formFieldService {
+    constructor(vendorDataService, constants, _) {
         'ngInject';
 
         //deps
         this.vendorDataService = vendorDataService;
         this._ = _;
         this.states = constants.states;
-
-        let states = constants.states;
-
-        this.nameFormField = [{
-            key            : 'name',
-            type           : 'input',
-            templateOptions: {
-                label   : 'Vendor Name',
-                requried: true
-            },
-            validators     : {
-                alpha: ($viewValue, $modelValue) => {
-                    let value = $modelValue || $viewValue;
-                    if(value.length > 1 && value.length < 60) {
-                        return /^[a-zA-Z ',-]*$/.test(value);
-                    } else {
-                        return false;
-                    }
-                }
-            }
-        }];
-        this.addressFormField = [
-            {
-                key            : 'address',
-                type           : 'input',
-                templateOptions: {
-                    label: 'Address',
-                    requried: true
-                },
-                validators     : {
-                    address: ($viewValue, $modelValue) => {
-                        let value = $modelValue || $viewValue;
-                        if(value.length > 1 && value.length < 150) {
-                            return /^[a-z A-Z 0-9 .#,-]*$/.test(value);
-                        } else {
-                            return false;
-                        }
-                    }
-                }
-            },
-            {
-                key            : 'city',
-                type           : 'input',
-                templateOptions: {
-                    label: 'City',
-                    requried: true
-                },
-                validators     : {
-                    city: ($viewValue, $modelValue) => {
-                        let value = $modelValue || $viewValue;
-                        if(value.length > 1 && value.length < 150) {
-                            return /^[a-z A-Z]*$/.test(value);
-                        } else {
-                            return false;
-                        }
-                    }
-                }
-
-            },
-            {
-                key            : 'state',
-                type           : 'select',
-                templateOptions: {
-                    label: 'State',
-                    requried: true,
-                    options: function(){
-                        let arr = [];
-                        _.each(states, (state, code)=>{
-                            arr.push({name:code, value:code});
-                        })
-                        return arr;
-                    }()
-                }
-            },
-            {
-                key            : 'zip_code',
-                type           : 'input',
-                templateOptions: {
-                    label: 'Zip Code',
-                    requried: true
-                },
-                validators     : {
-                    zip: ($viewValue, $modelValue) => {
-                        let value = $modelValue || $viewValue;
-                        if(value.length === 5) {
-                            return /^[0-9]*$/.test(value);
-                        } else {
-                            return false;
-                        }
-                    }
-                }
-            },
-            {
-                key            : 'phone_number',
-                type           : 'input',
-                templateOptions: {
-                    label: 'Phone Number',
-                    requried: true
-                },
-                validators     : {
-                    phone: ($viewValue, $modelValue) => {
-                        let value = $modelValue || $viewValue;
-                        if(value.length === 10) {
-                            return /^[0-9]*$/.test(value);
-                        } else {
-                            return false;
-                        }
-                    }
-                }
-            }
-
-
-        ];
-
     }
 
-    slugFormField (){
+    //slug form field
+    slugFormField () {
         return [{
             key            : '_slug',
             type           : 'input',
@@ -156,5 +43,125 @@ export class formFieldService{
             }
 
         }];
+    }
+
+    //name form field
+    nameFormField () {
+        return [{
+            key            : 'name',
+            type           : 'input',
+            templateOptions: {
+                label   : 'Vendor Name',
+                requried: true
+            },
+            validators     : {
+                alpha: ($viewValue, $modelValue) => {
+                    let value = $modelValue || $viewValue;
+                    if(value.length > 1 && value.length < 60) {
+                        return /^[a-zA-Z ',-]*$/.test(value);
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }];
+    }
+
+    //address form field
+    addressFormField () {
+        let _ = this._,
+            states = this.states;
+
+        return [
+            {
+                key            : 'address',
+                type           : 'input',
+                templateOptions: {
+                    label   : 'Address',
+                    requried: true
+                },
+                validators     : {
+                    address: ($viewValue, $modelValue) => {
+                        let value = $modelValue || $viewValue;
+                        if(value.length > 1 && value.length < 150) {
+                            return /^[a-z A-Z 0-9 .#,-]*$/.test(value);
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+            },
+            {
+                key            : 'city',
+                type           : 'input',
+                templateOptions: {
+                    label   : 'City',
+                    requried: true
+                },
+                validators     : {
+                    city: ($viewValue, $modelValue) => {
+                        let value = $modelValue || $viewValue;
+                        if(value.length > 1 && value.length < 150) {
+                            return /^[a-z A-Z]*$/.test(value);
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+
+            },
+            {
+                key            : 'state',
+                type           : 'select',
+                templateOptions: {
+                    label   : 'State',
+                    requried: true,
+                    options : function() {
+                        let arr = [];
+                        _.each(states, (state, code)=> {
+                            arr.push({name: code, value: code});
+                        })
+                        return arr;
+                    }()
+                }
+            },
+            {
+                key            : 'zip_code',
+                type           : 'input',
+                templateOptions: {
+                    label   : 'Zip Code',
+                    requried: true
+                },
+                validators     : {
+                    zip: ($viewValue, $modelValue) => {
+                        let value = $modelValue || $viewValue;
+                        if(value.length === 5) {
+                            return /^[0-9]*$/.test(value);
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+            },
+            {
+                key            : 'phone_number',
+                type           : 'input',
+                templateOptions: {
+                    label   : 'Phone Number',
+                    requried: true
+                },
+                validators     : {
+                    phone: ($viewValue, $modelValue) => {
+                        let value = $modelValue || $viewValue;
+                        if(value.length === 10) {
+                            return /^[0-9]*$/.test(value);
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+            }
+        ];
+
     }
 }
